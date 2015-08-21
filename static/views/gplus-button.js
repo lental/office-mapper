d3285d29e9854ad3d876722b6923f719b5f5e56a
Michael Len <michael.len@ooyala.com>
2015-08-21 14:47:35 -0700

    Actually have logout and API examples

--- static/views/gplus-button.js
+++ static/views/gplus-button.js
@@ -4,14 +4,60 @@ $(document).ready(function(){
       this.render();
     },
 
+    events: {
+      "click .logout": "logout",
+      "click .updateUsers": "updateUsers"
+    },
+
+    updateUsers: function() {
+      gapi.client.load('admin', 'directory_v1', function() {
+              // Step 5: Assemble the API request
+              console.log(gapi.client);
+              var request = gapi.client.directory.users.list({
+                customer: "my_customer",
+                viewType: "domain_public"
+              });
+              // Step 6: Execute the API request
+              request.execute(function(resp) {
+                console.log('RESPONSE', resp);
+              });
+            });
+    },
+    logout: function() {
+      gapi.load('auth2', function(){ 
+        gapi.auth2.getAuthInstance().signOut();
+      });
+    },
+
     el: '#edit-bar',
 
     render: function() {
-        
-      this.$el.html("<div class='g-signin2' data-onsuccess='onSignIn'></div>");
       return this;
     }
   });
-
   new GPlusButtonView({model:places});
 });
+
+function onSuccess(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail());
+  console.log('ID Token: ' + googleUser.getAuthResponse().id_token);
+
+}
+function onFailure(error) {
+  console.log(error);
+}
+function renderButton() {
+  gapi.signin2.render('my-signin2', {
+    'scope': 'https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/admin.directory.user.readonly',
+    'width': 250,
+    'height': 50,
+    'longtitle': true,
+    'theme': 'dark',
+    'onsuccess': onSuccess,
+    'onfailure': onFailure
+  });
+}
