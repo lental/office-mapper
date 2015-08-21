fc2083bd14ebb1a264b3b2128c39d6c1a52233f5
Michael Len <michael.len@ooyala.com>
2015-08-21 10:28:23 -0700

    update some styles and json

--- classes-fake-api.js
+++ classes-fake-api.js
@@ -1,7 +1,7 @@
 
 Users.prototype.url = "json/users.json";
 
-PointsOfInterest.prototype.url = "json/points-of-interest.json";
+PointsOfInterest.prototype.url = "json/places.json";
 
 Rooms.prototype.url = "json/rooms.json";
 
