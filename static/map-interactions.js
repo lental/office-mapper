c3882bed6e360ea3792cc47335ca9583ad422fe1
Dustin Preuss <dustin@ooyala.com>
2015-08-21 15:33:19 -0700

    the beginnings of a UI\!

--- static/map-interactions.js
+++ static/map-interactions.js
@@ -1,10 +1,28 @@
+var MapSectionView = Backbone.View.extend({
+  tagName: "div",
+  className: "section",
+  initialize: function() {
+   this.render(); 
+  },
+  template: _.template("<div class='sectionName'><%= name %></div>"),
+  render: function() {
+    this.$el.html(this.template(this.model.attributes));
+    this.$el.css({
+      height: this.model.attributes.position.h,
+      width: this.model.attributes.position.w,
+      top: this.model.attributes.position.y,
+      left: this.model.attributes.position.x
+    });
+    return this;
+  }
+});
+
 function onMapsReady() {
   console.log("Maps ready!");
   var defaultMap = maps.first();
-    
+  $("#map_name").text(defaultMap.attributes.name);
+  var sections = defaultMap.attributes.sections;
+  sections.forEach(function(section){
+    $("#map").prepend((new MapSectionView({model: section})).el);
+  });
 }
-
-window.addEventListener("DOMContentLoaded",   function(){
-    
-    
-});
