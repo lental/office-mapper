01ceb59891e36074a721df5f1e7c6e3cad183ea7
Evan Danaher <edanaher@ooyala.com>
2015-08-22 10:58:57 -0700

    server: Use loadAll for maps, places, and rooms routes.

--- src/office-mapper/data/data.go
+++ src/office-mapper/data/data.go
@@ -6,15 +6,15 @@ import (
 )
 
 type Position struct {
-	X int `json:"x"`
-	Y int `json:"y"`
-	W int `json:"w"`
-	H int `json:"h"`
+	X int `json:"x" sql:"xpos"`
+	Y int `json:"y" sql:"ypos"`
+	W int `json:"w" sql:"width"`
+	H int `json:"h" sql:"height"`
 }
 
 type XyPosition struct {
-	X int `json:"x"`
-	Y int `json:"y"`
+	X int `json:"x" sql:"xpos"`
+	Y int `json:"y" sql:"ypos"`
 }
 
 type Map struct {
@@ -94,16 +94,10 @@ type Desk struct {
 }
 
 func Maps() ([]Map, error) {
-	rows, err := config.DB.Query(`SELECT id, name FROM maps`)
+	maps := []Map{}
+	err := loadAll(&maps)
 	if err != nil {
 		return nil, err
-	}
-
-	maps := []Map{}
-	for rows.Next() {
-		m := Map{}
-		rows.Scan(&m.Id, &m.Name)
-		maps = append(maps, m)
 	}
 
 	return maps, nil
@@ -281,33 +275,20 @@ func UpdateUser(id int, u User) error {
 }
 
 func Rooms() ([]Room, error) {
-	rows, err := config.DB.Query(`SELECT id, name, section_id, xpos, ypos, width, height, chromecast, phone, tv,
- seats FROM rooms`)
+	rooms := []Room{}
+	err := loadAll(&rooms)
 	if err != nil {
 		return nil, err
-	}
-
-	rooms := []Room{}
-	for rows.Next() {
-		r := Room{}
-		rows.Scan(&r.Id, &r.Name, &r.SectionId, &r.Position.X, &r.Position.Y, &r.Position.W, &r.Position.H, &r.Features.Chromecast, &r.Features.Phone, &r.Features.Tv, &r.Features.Seats)
-		rooms = append(rooms, r)
 	}
 
 	return rooms, nil
 }
 
 func Places() ([]Place, error) {
-	rows, err := config.DB.Query(`SELECT id, name, section_id, description, xpos, ypos, width, height FROM places`)
+	places := []Place{}
+	err := loadAll(&places)
 	if err != nil {
 		return nil, err
-	}
-
-	places := []Place{}
-	for rows.Next() {
-		p := Place{}
-		rows.Scan(&p.Id, &p.Name, &p.SectionId, &p.Description, &p.Position.X, &p.Position.Y, &p.Position.W, &p.Position.H)
-		places = append(places, p)
 	}
 
 	return places, nil
