edc142dba332f6db99adffde53091e3c3b30dd74
Michael Len <michael.len@ooyala.com>
2015-08-21 19:38:33 -0700

    add form inputs for edit form and Section

--- static/views/edit-form.js
+++ static/views/edit-form.js
@@ -1,29 +1,56 @@
-var editUserTemplate = _.template("<div class='position-form'>" +
-  "<div class='edit-userId'>userId: <%= id %> </div>" +
+var editUserTemplate = _.template("<div class='editUserForm'><form>" +
+  "<div class='edit-id'>id: <%= id %> </div>" +
   "<div class='edit-name'>Name: <%= name %> </div>" +
   "<div class='edit-email'>email: <%= email %> </div>" +
-  "<div class='edit-deskId'>deskId: <%= deskId %> </div>" +
-  "</div>"
-  ); 
+  "<div class='edit-deskId'>deskId: <input type='text' name='deskId' value='<%= deskId %>'> </div>" +
+  "</form></div>"
+  );
 
-var editRoomTemplate = _.template("<div class='position-form'>" +
-  "<div class='edit-roomId'>roomId: <%= id %> </div>" +
-  "<div class='edit-name'>Name: <%= name %> </div>" +
-  "<div class='edit-features'>features: <%= JSON.stringify(features) %> </div>" +
-  "<div class='edit-sectionId'>sectionId: <%= sectionId %> </div>" +
-  "<div class='edit-position'>position: <%= JSON.stringify(position) %> </div>" +
+var editPositionTemplate = _.template("<div class='editPositionForm'>" +
+  "<div class='edit-x'>x: <input type='text' name='x' value='<%= x %>'> </div>" +
+  "<div class='edit-y'>y: <input type='text' name='y' value='<%= y %>'> </div>" +
+  "<div class='edit-w'>w: <input type='text' name='w' value='<%= w %>'> </div>" +
+  "<div class='edit-h'>h: <input type='text' name='h' value='<%= h %>'> </div>" +
   "</div>"
-  );                   
+  );
+
+var editFeaturesTemplate = _.template("<div class='editPositionForm'>" +
+  "<div class='edit-chromecast'>chromecast: <input type='checkbox' name='chromecast' value='chromecast' <%= chromecast ? 'checked' : '' %>> </div>" +
+  "<div class='edit-phone'>phone: <input type='checkbox' name='phone' value='phone' <%= phone ? 'checked' : '' %>> </div>" +
+  "<div class='edit-tv'>tv: <input type='checkbox' name='tv' value='tv' <%= tv ? 'checked' : '' %>> </div>" +
+  "<div class='edit-seats'>seats: <input type='text' name='seats' value='<%= seats %>'> </div>" +
+  "</div>"
+  );
+
+var editRoomTemplate = _.template("<div class='editRoomForm'><form>" +
+  "<div class='edit-id'>id: <%= id %> </div>" +
+  "<div class='edit-name'>Name: <input type='text' name='name' value='<%= name %>'> </div>" +
+  "<div class='edit-features'>features: <%= editFeaturesTemplate(features) %> </div>" +
+  "<div class='edit-sectionId'>sectionId: <input type='text' name='sectionId' value='<%= sectionId %>'> </div>" +
+  "<div class='edit-position'>position: <%= editPositionTemplate(position) %> </div>" +
+  "</form></div>"
+  );
+
+var editPlaceTemplate = _.template("<div class='editPlaceForm'><form>" +
+  "<div class='edit-id'>id: <%= id %> </div>" +
+  "<div class='edit-name'>Name: <input type='text' name='name' value='<%= name %>'></div>" +
+  "<div class='edit-description'>description: <input type='text' name='description' value='<%= description %>'> </div>" +
+  "<div class='edit-sectionId'>sectionId: <input type='text' name='sectionId' value='<%= sectionId %>'> </div>" +
+  "<div class='edit-position'>position: <%= editPositionTemplate(position) %> </div>" +
+  "</form></div>"
+  );
+
+var editSectionTemplate = _.template("<div class='editSectionForm'><form>" +
+  "<div class='edit-id'>id: <%= id %> </div>" +
+  "<div class='edit-name'>Name: <input type='text' name='name' value='<%= name %>'> </div>" +
+  // "<div class='edit-mapId'>mapId: <%= mapId %> </div>" +
+  "<div class='edit-position'>position: <%= editPositionTemplate(position) %> </div>" +
+  "</form></div>"
+  );
 
 
-var editPlaceTemplate = _.template("<div class='position-form'>" +
-  "<div class='edit-roomId'>roomId: <%= id %> </div>" +
-  "<div class='edit-name'>Name: <%= name %> </div>" +
-  "<div class='edit-description'>description: <%= description %> </div>" +
-  "<div class='edit-sectionId'>sectionId: <%= sectionId %> </div>" +
-  "<div class='edit-position'>position: <%= JSON.stringify(position) %> </div>" +
-  "</div>"
-  );  
+
+
 var EditFormView = Backbone.View.extend({
   initialize: function(){
     this.render();
@@ -49,6 +76,8 @@ var EditFormView = Backbone.View.extend({
       this.$el.html(editRoomTemplate(obj.attributes));
     } else if (obj instanceof Place) {
       this.$el.html(editPlaceTemplate(obj.attributes));
+    } else if (obj instanceof Section) {
+      this.$el.html(editSectionTemplate(obj.attributes));
     }
     else {
       this.$el.html("");
