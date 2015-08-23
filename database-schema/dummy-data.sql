ed8b244666fafac6f60908f5284fb1da7aedf780
Evan Danaher <edanaher@ooyala.com>
2015-08-22 19:41:41 -0700

    database-schema: Set dummy data so Abel Rios is in Dallas.

--- database-schema/dummy-data.sql
+++ database-schema/dummy-data.sql
@@ -1,8 +1,9 @@
-INSERT INTO maps (id, name) VALUES (1, "Santa Clara"), (2, "Guadalajara"), (3, "London"), (4, "Stockholm");
+INSERT INTO maps (id, name) VALUES (1, "Santa Clara"), (2, "Guadalajara"), (3, "London"), (4, "Stockholm"), (5, "Dallas");
 
 INSERT INTO sections (id, name, map_id, xpos, ypos, width, height) VALUES
     (1, "Engineering Area", 1, 0, 0, 600, 600),
-    (2, "Non-engineering Area", 1, 0, 650, 600, 300);
+    (2, "Non-engineering Area", 1, 0, 650, 600, 300),
+    (3, "Dallas Office", 2, 0, 0, 400, 500);
 
 INSERT INTO users (id, name, desk_id, email, thumbnail_url, gplus_id, admin) VALUES
   (1, "Michael Len", 1, "michael.len@ooyala.com", "https://plus.google.com/_/focus/photos/public/AIbEiAIAAABECNn4_9WI4pz-nwEiC3ZjYXJkX3Bob3RvKigwMWJlMmFjZjk2YWFlMzkyODQ1MmZmYzc4OTQ1ZTQ0Y2UzMDM1MWRjMAHNy1tiC-4vJNOiL2aadmRRetRhNw", 111528215661046135897, 1),
@@ -11,7 +12,7 @@ INSERT INTO users (id, name, desk_id, email, thumbnail_url, gplus_id, admin) VAL
   (4, "Aakash Desai", 5, "aakashd@ooyala.com", "https://plus.google.com/_/focus/photos/public/AIbEiAIAAABDCK-D8sSEooTkOiILdmNhcmRfcGhvdG8qKDUzOWQ5ODAzMDYzZjEzZDY4YzE1NWEwZTBjNjQ3MGU5ZWY3NmRkNWEwASoMw93ae0IKgnvra5L_pssOEYYh", 104235654211177316783, 0),
   (5, "Aanal Bhatt", 6, "aanalbhatt@ooyala.com", "https://plus.google.com/_/focus/photos/public/AIbEiAIAAABECKT4mvKzvrbQrQEiC3ZjYXJkX3Bob3RvKigzY2I1ZmRjODI5MTBhOWRiNGM4MDJjZGEwY2YxYzU2ZDhlYWRhNmI1MAGXXraqMI1ApYCU3KX1M_6NJy5zsw", 112511239403580341284, 0),
   (6, "Aaron Carr", 7, "aaronc@ooyala.com", NULL, 105266251472533597991, 0),
-  (7, "Abel Rios", 8, "abelrios@ooyala.com", "https://plus.google.com/_/focus/photos/public/AIbEiAIAAABDCNmJl4Cunf-aSyILdmNhcmRfcGhvdG8qKDg3OWJlZDY2MWZlNTU5YmNjYTBhYzMxMWFhM2RjM2UyOGRiMmVmNmMwAYqy3f5l36WA_z-rEcMkNEEGpaoN",105419515812321281241, 0);
+  (7, "Abel Rios", 10, "abelrios@ooyala.com", "https://plus.google.com/_/focus/photos/public/AIbEiAIAAABDCNmJl4Cunf-aSyILdmNhcmRfcGhvdG8qKDg3OWJlZDY2MWZlNTU5YmNjYTBhYzMxMWFhM2RjM2UyOGRiMmVmNmMwAYqy3f5l36WA_z-rEcMkNEEGpaoN",105419515812321281241, 0);
 
 INSERT INTO rooms (id, name, section_id, xpos, ypos, width, height, tv, phone, chromecast, seats) VALUES
   (1, "Shawshank Redemption", 1, 10, 110, 20, 40, 1, 1, 1, 4),
@@ -26,7 +27,8 @@ INSERT INTO desk_groups (id, name, section_id, xpos, ypos) VALUES
   (1, NULL, 1, 10, 160),
   (2, "Cool kids", 1, 40, 120),
   (3, "HR or something", 2, 80, 20),
-  (4, NULL, 2, 100, 80);
+  (4, NULL, 2, 100, 80),
+  (5, "Dallas eng cluster", 3, 40, 40);
 
 INSERT INTO desks (id, name, desk_group_id, xpos, ypos, width, height, rotation) VALUES
   (1, NULL, 1, 0, 0, 60, 20, 0),
@@ -40,4 +42,6 @@ INSERT INTO desks (id, name, desk_group_id, xpos, ypos, width, height, rotation)
   (7, "Nice HR", 3, 0, 60, 20, 0, 0),
   (8, "Mean HR", 3, 20, 60, 20, 0, 0),
 
-  (9, "Janky lonely small desk", 4, 0, 0, 50, 15, 0);
+  (9, "Janky lonely small desk", 4, 0, 0, 50, 15, 0),
+
+  (10, "Big Texas Desk", 5, 0, 0, 90, 30, 0);
