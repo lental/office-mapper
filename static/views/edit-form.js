28446f8fff71c5d43461ac1d836bd54bd5719405
Michael Len <michael.len@ooyala.com>
2015-08-22 19:40:38 -0700

    captialization and styling

--- static/views/edit-form.js
+++ static/views/edit-form.js
@@ -5,10 +5,10 @@ var formWrappingTemplate = _.template("<form>" +
   )
 
 var editUserTemplate = _.template("" +
-  "<tr class='formRow' id='edit-id'><td class='inputLabel'>id:</td><td class='inputField'><%= id %></td></tr>" +
+  "<tr class='formRow' id='edit-id'><td class='inputLabel'>Id:</td><td class='inputField'><%= id %></td></tr>" +
   "<tr class='formRow' id='edit-name'><td class='inputLabel'>Name:</td><td class='inputField'><%= name %></td></tr>" +
-  "<tr class='formRow' id='edit-email'><td class='inputLabel'>email:</td><td class='inputField'><%= email %></td></tr>" +
-  "<tr class='formRow' id='edit-deskId'><td class='inputLabel'>deskId:</td><td class='inputField'><input class='editInput' type='text' name='deskId' value='<%= deskId %>'></td></tr>"
+  "<tr class='formRow' id='edit-email'><td class='inputLabel'>Email:</td><td class='inputField'><%= email %></td></tr>" +
+  "<tr class='formRow' id='edit-deskId'><td class='inputLabel'>DeskId:</td><td class='inputField'><input class='editInput' type='text' name='deskId' value='<%= deskId %>'></td></tr>"
   );
 
 var editPositionTemplate = _.template("<table class='editPositionForm'>" +
@@ -20,34 +20,34 @@ var editPositionTemplate = _.template("<table class='editPositionForm'>" +
   );
 
 var editFeaturesTemplate = _.template("<table class='editFeatureForm'>" +
-  "<tr class='' id='edit-chromecast'><td class='featureInputLabel'>chromecast:</td><td class='featureInputField'><input class='editFeatInput' type='checkbox' name='chromecast' value='chromecast' <%= chromecast ? 'checked' : '' %>></td></tr>" +
-  "<tr class='' id='edit-phone'><td class='featureInputLabel'>phone:</td><td class='featureInputField'><input class='editFeatInput' type='checkbox' name='phone' value='phone' <%= phone ? 'checked' : '' %>></td></tr>" +
-  "<tr class='' id='edit-tv'><td class='featureInputLabel'>tv:</td><td class='featureInputField'><input class='editFeatInput' type='checkbox' name='tv' value='tv' <%= tv ? 'checked' : '' %>></td></tr>" +
-  "<tr class='' id='edit-seats'><td class='featureInputLabel'>seats:</td><td class='featureInputField'><input class='editFeatInput' type='text' name='seats' maxlength=3 value='<%= seats %>'></td></tr>" +
+  "<tr class='' id='edit-chromecast'><td class='featureInputLabel'>Chromecast:</td><td class='featureInputField'><input class='editFeatInput' type='checkbox' name='chromecast' value='chromecast' <%= chromecast ? 'checked' : '' %>></td></tr>" +
+  "<tr class='' id='edit-phone'><td class='featureInputLabel'>Phone:</td><td class='featureInputField'><input class='editFeatInput' type='checkbox' name='phone' value='phone' <%= phone ? 'checked' : '' %>></td></tr>" +
+  "<tr class='' id='edit-tv'><td class='featureInputLabel'>TV:</td><td class='featureInputField'><input class='editFeatInput' type='checkbox' name='tv' value='tv' <%= tv ? 'checked' : '' %>></td></tr>" +
+  "<tr class='' id='edit-seats'><td class='featureInputLabel'>Seats:</td><td class='featureInputField'><input class='editFeatInput' type='text' name='seats' maxlength=3 value='<%= seats %>'></td></tr>" +
   "</table>"
   );
 
 var editRoomTemplate = _.template("" +
-  "<tr class='formRow' id='edit-id'><td class='inputLabel'>id:</td><td class='inputField'><%= id %></td></tr>" +
+  "<tr class='formRow' id='edit-id'><td class='inputLabel'>Id:</td><td class='inputField'><%= id %></td></tr>" +
   "<tr class='formRow' id='edit-name'><td class='inputLabel'>Name:</td><td class='inputField'><input class='editInput' type='text' name='name' value='<%= name %>'></td></tr>" +
-  "<tr class='formRow' id='edit-features'><td class='inputLabel'>features:</td><td class='inputField'><%= editFeaturesTemplate(features) %></td></tr>" +
-  "<tr class='formRow' id='edit-sectionId'><td class='inputLabel'>sectionId:</td><td class='inputField'><input class='editInput' type='text' name='sectionId' value='<%= sectionId %>'></td></tr>" +
-  "<tr class='formRow' id='edit-position'><td class='inputLabel'>position:</td><td class='inputField'><%= editPositionTemplate(position) %></td></tr>"
+  "<tr class='formRow' id='edit-features'><td class='inputLabel'>Features:</td><td class='inputField'><%= editFeaturesTemplate(features) %></td></tr>" +
+  "<tr class='formRow' id='edit-sectionId'><td class='inputLabel'>Section Id:</td><td class='inputField'><input class='editInput' type='text' name='sectionId' value='<%= sectionId %>'></td></tr>" +
+  "<tr class='formRow' id='edit-position'><td class='inputLabel'>Position:</td><td class='inputField'><%= editPositionTemplate(position) %></td></tr>"
   );
 
 var editPlaceTemplate = _.template("" +
-  "<tr class='formRow' id='edit-id'><td class='inputLabel'>id:</td><td class='inputField'><%= id %></td></tr>" +
-  "<tr class='formRow' id='edit-name'>Name:<td class='inputLabel'></td><td class='inputField'><input class='editInput' type='text' name='name' value='<%= name %>'></td></tr>" +
-  "<tr class='formRow' id='edit-description'><td class='inputLabel'>description:</td><td class='inputField'><input class='editInput' type='text' name='description' value='<%= description %>'></td></tr>" +
-  "<tr class='formRow' id='edit-sectionId'><td class='inputLabel'>sectionId:</td><td class='inputField'><input class='editInput' type='text' name='sectionId' value='<%= sectionId %>'></td></tr>" +
-  "<tr class='formRow' id='edit-position'><td class='inputLabel'>position:</td><td class='inputField'><%= editPositionTemplate(position) %></td></tr>"
+  "<tr class='formRow' id='edit-id'><td class='inputLabel'>Id:</td><td class='inputField'><%= id %></td></tr>" +
+  "<tr class='formRow' id='edit-name'><td class='inputLabel'>Name:</td><td class='inputField'><input class='editInput' type='text' name='name' value='<%= name %>'></td></tr>" +
+  "<tr class='formRow' id='edit-description'><td class='inputLabel'>Description:</td><td class='inputField'><input class='editInput' type='text' name='description' value='<%= description %>'></td></tr>" +
+  "<tr class='formRow' id='edit-sectionId'><td class='inputLabel'>Section Id:</td><td class='inputField'><input class='editInput' type='text' name='sectionId' value='<%= sectionId %>'></td></tr>" +
+  "<tr class='formRow' id='edit-position'><td class='inputLabel'>Position:</td><td class='inputField'><%= editPositionTemplate(position) %></td></tr>"
   );
 
 var editSectionTemplate = _.template("" +
-  "<tr class='formRow' id='edit-id'><td class='inputLabel'>id:</td><td class='inputField'><%= id %></td></tr>" +
+  "<tr class='formRow' id='edit-id'><td class='inputLabel'>Id:</td><td class='inputField'><%= id %></td></tr>" +
   "<tr class='formRow' id='edit-name'><td class='inputLabel'>Name:</td><td class='inputField'><input class='editInput' type='text' name='name' value='<%= name %>'></td></tr>" +
   // "<tr class='formRow' id='edit-mapId'>mapId: <%= mapId %> </tr>" +
-  "<tr class='formRow' id='edit-position'><td class='inputLabel'>position:</td><td class='inputField'><%= editPositionTemplate(position) %></td></tr>"
+  "<tr class='formRow' id='edit-position'><td class='inputLabel'>Position:</td><td class='inputField'><%= editPositionTemplate(position) %></td></tr>"
   );
 
 var editMapTemplate = _.template(""+
