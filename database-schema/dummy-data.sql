e2b3f11f2feae069be6fcd2bede31e2aced83a07
Michael Len <michael.len@ooyala.com>
2015-08-23 01:52:22 -0700

    More Dummy data, one color change

--- database-schema/dummy-data.sql
+++ database-schema/dummy-data.sql
@@ -1,9 +1,10 @@
 INSERT INTO maps (id, name) VALUES (1, "Santa Clara"), (2, "Guadalajara"), (3, "London"), (4, "Stockholm"), (5, "Dallas");
 
 INSERT INTO sections (id, name, map_id, xpos, ypos, width, height) VALUES
-    (1, "Engineering Area", 1, 0, 0, 600, 600),
-    (2, "Non-engineering Area", 1, 0, 650, 600, 300),
-    (3, "Dallas Office", 2, 0, 0, 400, 500);
+    (1, "Engineering Area", 1, 10, 10, 1350, 400),
+    (2, "Walkway Area", 1, 10, 410, 1350, 400),
+    (3, "Non-engineering Area", 1, 10, 810, 1350, 400),
+    (4, "Dallas Office", 2, 0, 0, 400, 500);
 
 INSERT INTO users (id, name, desk_id, email, thumbnail_url, gplus_id, admin) VALUES
   (1, "Michael Len", 1, "michael.len@ooyala.com", "https://plus.google.com/_/focus/photos/public/AIbEiAIAAABECNn4_9WI4pz-nwEiC3ZjYXJkX3Bob3RvKigwMWJlMmFjZjk2YWFlMzkyODQ1MmZmYzc4OTQ1ZTQ0Y2UzMDM1MWRjMAHNy1tiC-4vJNOiL2aadmRRetRhNw", 111528215661046135897, 1),
@@ -15,11 +16,18 @@ INSERT INTO users (id, name, desk_id, email, thumbnail_url, gplus_id, admin) VAL
   (7, "Abel Rios", 10, "abelrios@ooyala.com", "https://plus.google.com/_/focus/photos/public/AIbEiAIAAABDCNmJl4Cunf-aSyILdmNhcmRfcGhvdG8qKDg3OWJlZDY2MWZlNTU5YmNjYTBhYzMxMWFhM2RjM2UyOGRiMmVmNmMwAYqy3f5l36WA_z-rEcMkNEEGpaoN",105419515812321281241, 0);
 
 INSERT INTO rooms (id, name, section_id, xpos, ypos, width, height, tv, phone, chromecast, seats) VALUES
-  (1, "Shawshank Redemption", 1, 10, 110, 20, 40, 1, 1, 1, 4),
-  (2, "Zamba", 2, 40, 90, 30, 80, 1, 1, 1, 20);
+  (1, "Shawshank Redemption", 1, 110, 20, 90, 90, 1, 1, 1, 4),
+  (2, "Gattaca", 1, 200, 20, 90, 90, 1, 1, 1, 4),
+  (3, "Men In Black", 1, 400, 200, 40, 40, 1, 1, 1, 4),
+  (4, "Time Bandits", 1, 440, 200, 40, 40, 1, 1, 1, 4),
+  (5, "Playback Lab", 1, 400, 240, 80, 50, 1, 1, 1, 4),
+  (6, "Wraith Of Khan", 1, 400, 290, 80, 50, 1, 1, 1, 4),
+  (7, "The Matrix", 1, 480, 200, 100,70, 1, 1, 1, 4),
+  (8, "Avatar", 1, 480, 270, 100,70, 1, 1, 1, 4),
+  (12, "Zamba", 2, 40, 90, 30, 80, 1, 1, 1, 20);
 
 INSERT INTO places (id, name, description, section_id, xpos, ypos, width, height) VALUES
-  (1, "Main kitchen", "Where we eat", 1, 200, 10, 100, 100),
+  (1, "Secondary kitchen", "Where we eat", 2, 40, 120, 100, 100),
   (2, "Electrical room", "Where electrons eat", 1, 50, 60, 10, 20),
   (3, "Bathroom", "Where baths eat", 2, 110, 110, 50, 50);
 
