fd713ac6e62c335a6bad2546e812744789f792b6
Michael Len <michael.len@ooyala.com>
2015-08-25 13:52:50 -0700

    fix bug when switching between maps

--- static/views/map/map-desk-view.js
+++ static/views/map/map-desk-view.js
@@ -3,12 +3,12 @@ var MapDeskView = Backbone.View.extend({
   className: "mapDesk shadowed",
   id: function() {return "map_desk_" + this.model.attributes.id;},
   initialize: function() {
+    this.$el.html(this.template({id:this.model.attributes.id}));
     this.render();
     this.user = users.getUserByDeskId(this.model.attributes.id);
     this.listenTo(this.model, 'sync', this.sync);
     this.listenTo(this.model, 'change', this.render);
     this.listenTo(gplus, 'change', this.render);
-    this.$el.html(this.template({id:this.model.attributes.id}));
   },
 
   sync: function(event) {
