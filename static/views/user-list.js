115829ce426152cf5194ab94abb31afdb484843b
Michael Len <michael.len@ooyala.com>
2015-08-25 13:47:47 -0700

    click Desk to find user

--- static/views/user-list.js
+++ static/views/user-list.js
@@ -13,7 +13,11 @@ var UserListView = Backbone.View.extend({
   initialize: function(){
     this.listenTo(pageState, 'change', this.render);
     this.hiding = false;
+    this.initialRender();
     this.render();
+  },
+  initialRender: function() {
+    this.$('#user-list').html(this.template({users:this.model}));
   },
 
   el: '#user-section',
@@ -46,25 +50,39 @@ var UserListView = Backbone.View.extend({
   template: _.template("<% users.each( function(user) { %>" +
        "<% var isSelected = user == pageState.get('selectedObject') %>" +
        "<% var params = JSON.parse(JSON.stringify(user.attributes))%>" +
-       "<% if (isSelected || user.searchMatches(pageState.get('searchQuery'))) { %>" +
        "<% params.isSelected = isSelected %>" +
        "<% params.mapName = user.get('mapId') ? maps.getMap(user.get('mapId')).get('name') : 'No Assigned Desk' %>" +
        "<%= userTemplate(params) %>" +
-    "<% }}); %> "),
+    "<% }); %> "),
 
   render: function() {
     this.$('.listHideButton').html(this.hiding ? "Show" : "Hide");
     this.$('#user-list').toggleClass("hiddenList", this.hiding);
-    this.$('#user-list').html(this.template({users:this.model}));
+
+
     var selectedObject = pageState.get('selectedObject');
-    if (!this.hiding) {
-      if (selectedObject instanceof User) {
-        var element = this.$('#user-list .listElement[data-id='+selectedObject.get('id')+']');
+    var selectedUser;
+    if (selectedObject instanceof User) {
+      selectedUser = selectedObject;
+    } else if (selectedObject instanceof Desk) {
+      selectedUser = this.model.getUserByDeskId(selectedObject.get('id'));
+    }
+
+    this.$('#user-list .listElement').each(_.bind(function(i, o){
+      var userForRow = users.getUser(o.dataset.id);
+      var isSelected = userForRow == selectedUser;
+      // if () { 
+      $(o).toggleClass("displayNone", (this.isHiding ||!(isSelected || userForRow.searchMatches(pageState.get('searchQuery')))));
+      $(o).toggleClass("active", isSelected);
+      if(isSelected) {
+        var element = this.$('#user-list .listElement[data-id='+selectedUser.get('id')+']');
         if (isChildPartiallyOutsideOfParent(element[0], $("#scrollable-list")[0])) {
           element[0].scrollIntoView();
         }
       }
-    }
+      // }
+    },this));
+
     return this;
   }
 });
