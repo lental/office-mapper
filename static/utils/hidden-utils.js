b0355967516e63b378bcc5e0952ca8d45398e988
Michael Len <michael.len@ooyala.com>
2015-08-25 11:08:43 -0700

    fix hidden-utils, again

--- static/utils/hidden-utils.js
+++ static/utils/hidden-utils.js
@@ -1,41 +1,19 @@
 function isChildPartiallyOutsideOfParent(child, parent){
 
-  absChild = { offsetLeft:child.offsetLeft,
-               offsetTop:child.offsetTop,
-               offsetWidth:child.offsetWidth,
-               offsetHeight:child.offsetHeight
-             };
-  curNode = child.parentElement;
-  while (curNode != null) {
-    if (curNode == parent) {
-      break;
-    } else {
-      absChild.offsetLeft += curNode.offsetLeft;
-      absChild.offsetTop += curNode.offsetTop;
-      curNode = curNode.parentElement;
-    }
-  }
-  if(curNode == null) {
-    console.log("Child is not a descendant of Parent");
-    return null;
-  }
-return isChildOffLeftOfParent(absChild, parent) ||
-       isChildOffRightOfParent(absChild, parent) ||
-       isChildOffTopOfParent(absChild, parent) ||
-       isChildOffBottomOfParent(absChild, parent);
+return isChildOffLeftOfParent(child.getBoundingClientRect(), parent.getBoundingClientRect()) ||
+       isChildOffRightOfParent(child.getBoundingClientRect(), parent.getBoundingClientRect()) ||
+       isChildOffTopOfParent(child.getBoundingClientRect(), parent.getBoundingClientRect()) ||
+       isChildOffBottomOfParent(child.getBoundingClientRect(), parent.getBoundingClientRect());
 }
 function isChildOffLeftOfParent(child, parent) {
-  return child.offsetLeft  < parent.scrollLeft;
+  return child.left  < parent.left;
 }
 function isChildOffRightOfParent(child, parent) {
-  return child.offsetLeft  + child.offsetWidth >
-      parent.scrollLeft + parent.offsetWidth;
+  return child.right  > parent.right;
 }
 function isChildOffTopOfParent(child, parent) {
-  return child.offsetTop < parent.scrollTop;
-  
+  return child.top  < parent.top;
 }
 function isChildOffBottomOfParent(child, parent) {
-  return child.offsetTop + child.offsetHeight >
-      parent.scrollTop + parent.offsetHeight;
+  return child.bottom  > parent.bottom;
 }
